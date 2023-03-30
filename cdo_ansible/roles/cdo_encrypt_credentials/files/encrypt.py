25dc266fa52730c6f40c9f24700e2af4f7205194
aaronhackney <ahack210@gmail.com>
2023-03-30 09:29:05 -0400

    dynamic library load

--- cdo_ansible/roles/cdo_encrypt_credentials/files/encrypt.py
+++ cdo_ansible/roles/cdo_encrypt_credentials/files/encrypt.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
 # Requires: pycryptodome
+import pip
 import sys
 import json
 import base64
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_v1_5
 
-
+def load_package(lib_test, package):
+    try:
+        print("Attempting to import needed library...")
+        __import__(lib_test)
+    except ImportError:
+        print(f"Library {lib_test} not found attempting to install...")
+        pip.main(['install', package])
+    
 def main(username, password, public_key):
     key = RSA.importKey(base64.b64decode(public_key))
     encryptor = PKCS1_v1_5.new(key)
@@ -24,4 +32,5 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     public_key = sys.argv[3]
+    load_package("Crypto.PublicKey", "pycryptodome")
     main(username, password, public_key)
