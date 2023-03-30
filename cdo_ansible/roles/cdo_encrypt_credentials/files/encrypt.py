c152079d043cc6f4cbc538620e41facbec96c525
aaronhackney <ahack210@gmail.com>
2023-03-30 10:05:00 -0400

    dynamic library load

--- cdo_ansible/roles/cdo_encrypt_credentials/files/encrypt.py
+++ cdo_ansible/roles/cdo_encrypt_credentials/files/encrypt.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 # Requires: pycryptodome
-import pip
+import os
 import sys
 import json
 import base64
 
 def load_package(lib_test, package):
     try:
-        print("Attempting to import needed library...")
         __import__(lib_test)
     except ImportError:
-        print(f"Library {lib_test} not found attempting to install...")
-        pip.main(['install', package])
+        os.system(f"{sys.executable} -m pip install {package}")
     
 def main(username, password, public_key):
     key = RSA.importKey(base64.b64decode(public_key))
